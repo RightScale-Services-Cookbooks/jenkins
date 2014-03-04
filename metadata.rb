185b63ac455e100003de66e0c7dac57f71383720
Stefhen Hovland <stefhen@rightscale.com>
2014-03-04 14:52:08 -0500

    Typo

--- metadata.rb
+++ metadata.rb
@@ -28,7 +28,7 @@ recipe "jenkins::do_attach_slave_at_boot",
 attribute "jenkins/server/home",
   :display_name => "Directory where Jenkins builds will take place",
   :description => "All builds will take place here",
-  :required => "suggested",
+  :required => "recommended",
   :recipes => [ "jenkins::default", "jenkins::install_server", 
     "jenkins::do_attach_request", "jenkins::do_attach_slave_at_boot" ],
   :default => "/mnt/ephemeral"
